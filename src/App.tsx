7c6573b90f2da5e6781df9bb97a275855b04e7c1
Martin Racana <ing.racana@gmail.com>
2024-06-19 15:46:58 +0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="max-w-lg w-full">
+      <header>
+        <h1>My TODO</h1>
+      </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
